005cb58df868ce92955aac181fb8edb48ee42fac
alex006-cloud <74815063+alex006-cloud@users.noreply.github.com>
2022-04-27 14:47:49 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 import setuptools
+import pathlib
+
+
+def read(file: str) -> list:
+    with open(file, encoding="utf-8") as r:
+        return [i.strip() for i in r]
+
+
+file = pathlib.Path(__file__).parent
+
+README = (file / "README.md").read_text()
 
 setuptools.setup(
     name="pytube",
-    version="0.2.1",
+    version="0.2.2",
     author="AlexGmB",
     author_email="None",
     description="Python library Get YouTube Video Data",
@@ -14,5 +25,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
+    install_requires = read(""),
     python_requires=">=3.6"
 )
