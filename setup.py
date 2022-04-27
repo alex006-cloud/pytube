509ed0d9525f0c7791ac0f4f54e2dfa84f05e9d6
alex006-cloud <74815063+alex006-cloud@users.noreply.github.com>
2022-04-27 14:06:19 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="pytube",
+    version="0.0.1",
+    author="AlexGmB",
+    author_email="None",
+    description="Python library Get YouTube Video Data",
+    license="MIT",
+    url="https://github.com/alex006-cloud/pytube",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires = read(""),
+    python_requires=">=3.6"
+)
