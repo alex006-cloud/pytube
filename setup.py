286ed5cc7459c3bef7460c3ea0b8dd4afe7601d8
alex006-cloud <74815063+alex006-cloud@users.noreply.github.com>
2022-04-29 18:35:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setuptools.setup(
     version="1.2.1",
     author="mrlokaman&AlexGmd",
     author_email="ln0technical@gmail.com",
-    long_description = README,
     description="Python library Get YouTube Video Data",
     license="MIT",
     url="https://github.com/alex006-cloud/pytube",
