d3df68c7bbd7359334bb96f4f2e003e49f65ae6e
alex006-cloud <74815063+alex006-cloud@users.noreply.github.com>
2022-04-29 16:53:21 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+import setuptools
+
+
+def read(file: str) -> list:
+    with open(file, encoding="utf-8") as r:
+        return [i.strip() for i in r]
+
+
+file = pathlib.Path(__file__).parent
+
+README = (file / "README.md").read_text()
+
+setuptools.setup(
+    name="pytube",
+    version="1.1.3",
+    author="mrlokaman&AlexGmd",
+    author_email="ln0technical@gmail.com",
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    description="Python library Get YouTube Video Data",
+    license="MIT",
+    url="https://github.com/alex006-cloud/pytube",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires = read(""),
+    python_requires=">=3.6"
+)
